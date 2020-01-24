e9b4440b3bdc5ccc501c4cee3f1365376663328f
Karan <kumarkay.ca@>
2020-01-24 18:55:53 -0500

    init

---
+++ Final_Lab_Assignment/Products.swift
@@ -0,0 +1,22 @@
+//
+//  Products.swift
+//  Final_Lab_Assignment
+//
+//  Created by MacStudent on 2020-01-24.
+//  Copyright © 2020 Karan. All rights reserved.
+//
+
+import Foundation
+class Products {
+    internal init(ProductName: String, Product_Description: String, Product_Price: Int, Product_Id: Int) {
+        self.Product_Name = ProductName
+        self.Product_Description = Product_Description
+        self.Product_Price = Product_Price
+        self.Product_Id = Product_Id
+    }
+    
+      var Product_Name : String
+      var Product_Description: String
+      var Product_Price : Int
+      var Product_Id : Int
+}
